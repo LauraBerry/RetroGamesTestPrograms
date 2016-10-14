29b15af2eae31b75fe1dc741bd1142f7ce147ade
lnberry <lnberry@csx3.cs.ucalgary.ca>
2016-10-14 10:56:10 -0600

    it doesn't like the branch instructions for some reason i'm working on why

--- randNum.asm
+++ randNum.asm
@@ -1,19 +1,11 @@
-//Input 
-//R0 = start number.
-//note: this is psuedo random and is taken from here (https://en.wikipedia.org/wiki/Wichmann-Hill)
-
-.section    .init
-.global     _start
-
-_start:
-    b       randNum
-    
-.section .text
+;Input 
+;R0 = start number. R1== secondary number R2 == third number
+;note: this is psuedo random and is taken from here (https://en.wikipedia.org/wiki/Wichmann-Hill)
 
 randNum:
 	JMP main
-	MOV R3, R6
-	MOV R4, #0
+	MOV R6, R3
+	MOV 0,  R4
 	loopStart:
 		SUB R3, 171, R3
 		CMP R3, R6
@@ -21,22 +13,22 @@ randNum:
 		INC R4
 nextStep:
 	ADD R4, R4
-	MOV R5, #0
+	MOV 0, R5
 	loop2start:
 		ADD R3, R3
 		INC R5
-		CMP R5, #171
+		CMP R5, 171
 		BNE	loop2start
 	SUB R3, R3, R4
 main:
-	MOV R6, R0
+	MOV R0, R6
 	JMP randNum
-	MOV R7, R3
-	MOV R6, R1
+	MOV R3, R7
+	MOV R1, R6
 	JMP randNum
 	ADD R7, R3, R7
-	MOV R6, R2
-	JUMP randNum
+	MOV R2, R6
+	JMP randNum
 	ADD R7, R3, R7
 
 	
