89c8c9426860e7012358599b7feee0d9d6bdb0e2
lnberry <lnberry@csx1.cs.ucalgary.ca>
2016-10-16 14:13:55 -0600

    changed to new stub and changed memory location of COLORMAP

--- 09_Timing_Test/waitLoop.asm
+++ 09_Timing_Test/waitLoop.asm
@@ -1,24 +1,35 @@
 ;
 ; VIC20 Test Program
-; Wait Loop
+; <Program Title>
 ; (C) 2016 by Konrad Aust, Laura Berry, Andrew Lata, Yue Chen
 ; 
-; waits and then changes the color of the screen.
+; <Program Descripion>
 ;
 
 ; ************* Program Constants ****************
 CLRSCN  = $e55f
-COLORMAP = $0286					;location of colors in vic 20
+COLORMAP = $38400
 
 
 ; ************* Assembly Code ***************
 
-	processor 6502
-	org 4097
+;DASM VIC20 BASIC stub --------------------------------------|
+          processor 6502
+          org 4097		   	;4097 ; start of program area
 
-	; BASIC stub we use to launch our assembly
-	; 158 = 9E = "SYS" token
-	byte 11,16,214,7,158,"1","3","3","7",0,0,0
+;labels
+chrout  = $ffd2
+
+basicStub: 
+		dc.w basicEnd		; 4 byte pointer to next line of basic
+		dc.w 2013		; 4 byte (can be any number for the most part)
+		hex  9e			; 1 byte Basic token for SYS
+		hex  20			; ascii for space = 32
+		hex  34 31 31 30	; hex for asci 4110
+		hex 00
+basicEnd:	hex 00 00        	; The next BASIC line would start here
+
+;End of DASM VIC20 BASIC stub ---------------------------------|
 
 init:	
 	LDA #00
