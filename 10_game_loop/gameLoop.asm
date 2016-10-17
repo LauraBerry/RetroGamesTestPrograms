52bb05ac3c7492d67689c04be119887cc8654c5a
lnberry <lnberry@csx1.cs.ucalgary.ca>
2016-10-17 12:16:42 -0600

    game loop, doesn't do much yet

---
+++ 10_game_loop/gameLoop.asm
@@ -0,0 +1,85 @@
+;
+; VIC20 Test Program
+; <Program Title>
+; (C) 2016 by Konrad Aust, Laura Berry, Andrew Lata, Yue Chen
+; 
+; <Program Descripion>
+;
+
+; ************* Program Constants ****************
+CLRSCN  = $e55f
+CURRSCORE= $0014
+
+
+
+; ************* Assembly Code ***************
+
+	processor 6502
+	org 4097
+
+	; BASIC stub we use to launch our assembly
+	; 158 = 9E = "SYS" token
+	byte 11,16,214,7,158,"1","3","3","7",0,0,0
+
+init:
+	;LDA #score
+	;STA CURRSCORE
+	LDA gameState
+	CMP #1
+	BNE nextLoop
+	;this should spell out gameover
+	LDA #$47
+	JSR $FFD2
+	LDA #$41
+	JSR $FFD2
+	LDA #$53
+	JSR $FFD2
+	LDA #$45
+	JSR $FFD2
+	LDA #$55
+	JSR $FFD2
+	LDA #$62
+	JSR $FFD2
+	LDA #$45
+	JSR $FFD2
+	LDA #$58
+	JSR $FFD2
+	LDY #00
+subLoop:        	
+    CPY #$ff
+	BNE subLoop
+	;jsr CLRSCN	 
+nextLoop:
+	;CPX #02
+	;BNE scoreLoop
+	;this should spell out startgame
+	LDA #$59
+	JSR $FFD2
+	LDA #$60
+	JSR $FFD2
+	LDA #$41
+	JSR $FFD2
+	LDA #$58
+	JSR $FFD2
+	LDA #$60
+	JSR $FFD2
+	;LDA #$00
+	;JSR $FFD2
+	LDA #$47
+	JSR $FFD2
+	LDA #$41
+	JSR $FFD2
+	LDA #$53
+	JSR $FFD2
+	LDA #$45
+	JSR $FFD2
+	;QUESTION MARK
+	jmp finish		
+finish:
+
+
+; **************** DATA Section ****************************
+
+gameState: byte 0 		;0== game running, 1== end game, 2== new game
+
+;include "../06_score/score_test.asm"
