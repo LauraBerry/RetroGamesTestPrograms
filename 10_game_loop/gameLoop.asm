7a94163713718a6e45ef2e385b5d1efa9a055ee0
lnberry <lnberry@csx2.cs.ucalgary.ca>
2016-10-18 12:46:37 -0600

    listens for a key press and changes the background color upon getting one

--- 10_game_loop/gameLoop.asm
+++ 10_game_loop/gameLoop.asm
@@ -3,101 +3,69 @@
 ; game loop
 ; (C) 2016 by Konrad Aust, Laura Berry, Andrew Lata, Yue Chen
 ; 
-; <Program Descripion>
+; checks memory location to see if player has touched the lava (died) and if so prints game over  new game Y N on the screen
 ;
 
 ; ************* Program Constants ****************
 CLRSCN  = $e55f
-CURRSCORE = $4100
-TOUCHEDLAVA = $4101
-
-
+COLORMAP = $900f		  ;background color
+AUXCOLOR = $9600		  ;aux color
+RDTIM = $FFDE             ; Read Clock Kernel Method
 
 ; ************* Assembly Code ***************
 
-	processor 6502
-	org 4097
+;DASM VIC20 BASIC stub --------------------------------------|
+          processor 6502
+          org 4097		   	;4097 ; start of program area
 
-	; BASIC stub we use to launch our assembly
-	; 158 = 9E = "SYS" token
-	byte 11,16,214,7,158,"1","3","3","7",0,0,0
+;labels
+chrout  = $ffd2
+
+basicStub: 
+		dc.w basicEnd		; 4 byte pointer to next line of basic
+		dc.w 2013		; 4 byte (can be any number for the most part)
+		hex  9e			; 1 byte Basic token for SYS
+		hex  20			; ascii for space = 32
+		hex  34 31 31 30	; hex for asci 4110
+		hex 00
+basicEnd:	hex 00 00        	; The next BASIC line would start here
+
+;End of DASM VIC20 BASIC stub ---------------------------------|
 
 turn:
-	;LDA #score
-	;STA CURRSCORE
-	LDA TOUCHEDLAVA
-	CMP #1
-	BEQ	jump
-	LDA CURRSCORE
-	ADC #1
-	STA CURRSCORE
-	JSR $FFD2
-	BEQ turn
-jump:
-	;this should spell out game over
-	LDA #$47
-	JSR $FFD2
-	LDA #$41
-	JSR $FFD2
-	LDA #$53
-	JSR $FFD2
-	LDA #$45
-	JSR $FFD2
-	LDA $0
-	JSR $FFD2
-	LDA #$55
-	JSR $FFD2
-	LDA #$62
-	JSR $FFD2
-	LDA #$45
-	JSR $FFD2
-	LDA #$58
-	JSR $FFD2
-	LDY #00
-subLoop:        	
-    CPY #$ff
-	BNE subLoop
-	;jsr CLRSCN	 
-	;this should spell out start game
-	LDA #$59
-	JSR $FFD2
-	LDA #$60
-	JSR $FFD2
-	LDA #$41
-	JSR $FFD2
-	LDA #$58
-	JSR $FFD2
-	LDA #$60
-	JSR $FFD2
-	LDA $0
-	JSR $FFD2
-	LDA #$47
-	JSR $FFD2
-	LDA #$41
-	JSR $FFD2
-	LDA #$53
-	JSR $FFD2
-	LDA #$45
-	JSR $FFD2
-	;QUESTION MARK
-	LDA $0
-	JSR $FFD2
-	LDA #65
-	JSR $FFD2
-	LDA $0
-	JSR $FFD2
-	LDA $0
-	JSR $FFD2
-	LDA #54
-	JSR $FFD2
-	jmp finish		
-finish:
+	JSR $FFC0 ;OPEN CHANNEL
+	JSR $FFC6	;CHECK IN CHANNEL
+	LDX #00
+	JSR $FFCF
+	JSR $FFE4
+	TXA
+	CPX $41
+	BNE red
+	LDA #00
+	STA COLORMAP
+	STA AUXCOLOR
+	JSR RDTIM     			 ;busy loop to make the program wait 3 seconds                     
+	JSR delay
+	jmp turn
+red:
+	LDA #02
+	STA COLORMAP
+	STA AUXCOLOR
+	JSR RDTIM     			 ;busy loop to make the program wait 3 seconds                     
+	JSR delay
+	jmp turn
+	
+delay:
+    JSR RDTIM               ; Read the time
+    ADC #10                 ; Add 10 to the MSB (Dunno how many 'jiffies' that is
+    STA next_inc      		; Put it in memory
+_wait_loop:                  ; This is weird and I'm not sure it's totally uniform
+    JSR RDTIM               ; Read the system timer
+    CMP next_inc      		; Check the time against our stored value
+    BNE _wait_loop           ; if time != next_increment, loop
+    RTS
+	
+	; **************** DATA Section ****************************
+next_inc: byte 0 
 
 
-; **************** DATA Section ****************************
-
-gameState: byte 0 		;0== game running, 1== end game, 2== new game
-
-
-
-;include "../06_score/score_test.asm"
