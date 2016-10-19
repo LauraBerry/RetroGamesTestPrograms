06165526e48074e4514e8723feb08ac36ae6ddf5
yuechen1 <yuechen1@live.com>
2016-10-18 21:39:49 -0600

    screen test files
    screen is working, just adding stuff to the first line then have it copy down in 03
    character movement is working, only last line is printing weird(can be fixed with a branch as its only that line), move with w,a,s,d  prints a new character at the location, game loop too fast so its print new lines but looks cool

---
+++ 03_screen/text.asm
@@ -0,0 +1,89 @@
+;
+; VIC20 Test Program 1
+; Clear the Screen
+; (C) 2016 by Konrad Aust, Laura Berry, Andrew Lata, Yue Chen
+; 
+; This program is to show that we can get assembly code running on the VIC20.
+;
+
+; **************** Program Constants ***********************
+
+screencolor = $9600		;colour space
+screencolor1 = $96ff	;colour space
+
+screen0 = $1E00			;screen location
+screen1 = $1F00			;screen location
+
+; **************** Assembly Stub ***************************
+
+    processor 6502          ; We're on the 6502 Processor
+    org 4097                ; Set the origin location of our code.
+
+;labels
+chrout = $ffd2
+
+basicStub: 
+		dc.w basicEnd		; 4 byte pointer to next line of basic
+		dc.w 2013		; 4 byte (can be any number for the most part)
+		hex  9e			; 1 byte Basic token for SYS
+		hex  20			; ascii for space = 32
+		hex  34 31 31 30	; hex for asci 4110
+		hex 00
+basicEnd:	hex 00 00        	; The next BASIC line would start here
+
+
+;***************** Assembly Code****************************
+;----------set screen colour	
+	LDA #00
+	LDX #$00
+color:
+	STA screencolor,x
+	STA screencolor1,x
+	INX
+	CPX #$ff
+	BNE color
+
+
+;--------generate 22 "random" 0(48) or 1(49), dont 
+	LDA #49
+	STA $1E16
+start:
+;------generate
+	LDX #$00
+	LDA $1E16
+	CMP #49
+	BEQ g1
+generate:
+	CMP #49
+	BNE g2
+g1:
+	LDA #48
+	JMP gp
+g2:
+	LDA #49
+gp:	
+	STA $1E00,x
+	INX
+	CPX #22
+	BNE generate
+	JSR screendown
+	JMP start
+	BRK
+	
+;-----------function to copy the screen down one row--------	
+screendown:	
+	LDX #$00
+screenloop:
+	LDA $1E00,x
+	STA $1E16,x	;1e16 is the second row
+	LDA $1EB0,x
+	STA $1EC6,x ;1F15 is 1e16 + ff
+	LDA $1F4A,x
+	STA $1F60,x 
+	INX
+	CPX #$B0
+	BNE screenloop
+	RTS
+	
+	
+	
---
+++ 04 character movement/text2.asm
@@ -0,0 +1,198 @@
+;
+; VIC20 Test Program 1
+; Clear the Screen
+; (C) 2016 by Konrad Aust, Laura Berry, Andrew Lata, Yue Chen
+; 
+; This program is to show that we can get assembly code running on the VIC20.
+;
+
+; **************** Program Constants ***********************
+        ; clear screen kernel method
+;A = #$11;   dec 17	
+;D = #$12;   dec 18
+;W = #$13;   dec 09
+;S = #$14;   dec 41
+Sscreen = #$1E00	;start of screen memory
+Sscreen1 = #$1F00	;start of second part of screen memory
+keypress = #$00C5	;read from here to get key press
+sln = #$16	;screen line spacing, add to go to the next line 
+zero_print = $F0;the character we are printing
+zero_x = $F1;the current x location
+zero_y = $F2;the current y location
+screencolor = $9600		;colour space
+screencolor1 = $96ff	;colour space
+; **************** Assembly Stub ***************************
+
+    processor 6502          ; We're on the 6502 Processor
+    org 4097                ; Set the origin location of our code.
+
+;labels
+chrout = $ffd2
+
+basicStub: 
+		dc.w basicEnd		; 4 byte pointer to next line of basic
+		dc.w 2013		; 4 byte (can be any number for the most part)
+		hex  9e			; 1 byte Basic token for SYS
+		hex  20			; ascii for space = 32
+		hex  34 31 31 30	; hex for asci 4110
+		hex 00
+basicEnd:	hex 00 00        	; The next BASIC line would start here
+;------------------------------------------------------------
+;----change the screen colour 
+	LDA #00
+	LDX #$00
+color:
+	STA screencolor,x
+	STA screencolor1,x
+	INX
+	CPX #$ff
+	BNE color
+
+	
+
+;------set player location
+	LDA #$01
+	STA zero_print
+	LDA #$0B
+	STA zero_y
+	LDA #$00
+	STA zero_x
+;--------movement loop
+move:
+	LDA keypress
+	CMP #17	;A
+	BEQ cleft
+	CMP #18	;D
+	BEQ cright
+	CMP #9	;S
+	BEQ cdown
+	CMP #41	;W
+	BEQ cup
+	JMP move
+cleft:
+	JMP left
+cright:
+	JMP right
+cdown:
+	JMP	down
+cup:
+	JMP up
+
+
+
+left:
+	LDX zero_x
+	CPX #$00
+	BEQ	leftat
+	DEX
+	STX zero_x
+	JMP leftend
+leftat:	
+	LDA #21
+	STA zero_x
+leftend:
+	JMP printcharacter
+
+
+right:
+	LDX zero_x
+	CPX #$15
+	BEQ	rightat
+	INX
+	STX zero_x
+	JMP rightend
+rightat:	
+	LDA #00
+	STA zero_x
+rightend:
+	JMP printcharacter
+
+	
+down:
+	LDX zero_y
+	CPX #$16 
+	BEQ downat
+	INX
+	STX zero_y
+	JMP downend
+downat:
+	LDA #$00
+	STA zero_y
+downend:
+	JMP printcharacter
+
+up:
+	LDX zero_y
+	CPX #$00
+	BEQ upat
+	DEX
+	STX zero_y
+	JMP upend
+upat:
+	LDA #$16
+	STA zero_y
+upend:
+	JMP printcharacter
+	
+	
+	
+;-------------------print the character at the new spot-----------------
+printcharacter:
+;check if at the break line	
+	LDA zero_y
+	CMP #$0B
+	BEQ atline
+;see if in first or second memory page	
+	CLC
+	SBC #$0B
+	BPL printscreen2
+;----at screen one, calculate the y offset	
+	LDA #$00
+	LDX zero_y
+pc1:	;loop to add
+	CPX #$00
+	BEQ pc1d
+	ADC #$15
+	DEX
+	JMP pc1
+pc1d:	;add x offset
+	ADC zero_x
+	TAX
+	DEX
+	LDA zero_print
+	STA $1E00,x
+	ADC #$01
+	STA zero_print
+	JMP move
+;----at screen two calculate the print position
+printscreen2: ;1EF2
+;get y location on the second table
+	TAX
+	LDA #$00
+pc2:
+	CPX #$0C
+	BEQ pc2d
+	ADC #$16
+	DEX
+	JMP pc2
+pc2d:
+	ADC zero_x
+	TAX
+	DEX
+	DEX
+	LDA zero_print
+	STA $1F08,x
+	ADC #$01
+	STA zero_print
+	JMP move
+;----on the fuck line, calculate the print position
+atline:
+	LDA zero_print	;get print character
+	LDX zero_x	;get x offset
+	STA $1EF2,x	;store to screen memory
+	ADC #$01
+	STA zero_print
+	JMP move
+
+	
+	BRK
